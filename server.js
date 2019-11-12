fb2bbe2bcdb42540b561f12b5af43c7b43d2c26d
whykatherine <whykatherine@gmail.com>
2019-11-12 15:25:45 -0800

    feat: click through to create chain of links

--- server.js
+++ server.js
@@ -1,25 +1,28 @@
-const request = require('request')
-const cheerio = require('cheerio')
+const request = require('request');
+const cheerio = require('cheerio');
 
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const parser = require('body-parser');
 
 // allow access to static files in public directory
 app.use(express.static('public'));
+// body parsing middleware, provides req.body
+app.use(parser.urlencoded({extended: true}));
 // use templating engine
 app.set('view engine', 'ejs');
 
 // get random wikipedia page
 var randomURL = "https://en.wikipedia.org/wiki/Special:Random#/random";
-var initialArticle = {};
 var chain = [];
 
-app.get('/', function(req, res) {
+function renderData(url, res) {
 
   // get contents of webpage
-  request(randomURL, function(error, response, body) {
+  request(url, function(error, response, body) {
 
     if (error) {
+
       res.render('index', {
         articleTitle: null, 
         articleLinks: null,
@@ -32,14 +35,11 @@ app.get('/', function(req, res) {
       // parse page
       var $ = cheerio.load(body);
 
-      // set up initial article object
-      initialArticle = {
-        title: $('#firstHeading').html(),
-        url: 'https://en.wikipedia.org' + response.socket._httpMessage.path
-      };
+      // get article heading
+      chain.push($('#firstHeading').html());
 
+      // get links on page (only those going to other wikipedia articles)
       var articleLinks = [];
-      // get all links that go to other wikipedia articles
       $(
         '#bodyContent a' + 
           '[href^="/wiki/"]' + 
@@ -55,13 +55,15 @@ app.get('/', function(req, res) {
             '[href^="/wiki/Help:"],' + 
             '.internal)'
       ).each(function(i, elem) {
-        var url = 'https://en.wikipedia.org' + $(this).attr('href');
         var title = $(this).attr('title');
+        var url = 'https://en.wikipedia.org' + $(this).attr('href');
+        var url_stub = $(this).attr('href').substring(6); // the part after /wiki/
         // get sentence/kwic?
         // add to links array (only if not duplicate?)
         articleLinks.push({
-          url: url,
           title: title,
+          url: url,
+          url_stub: url_stub
         });
       });
 
@@ -70,20 +72,31 @@ app.get('/', function(req, res) {
       // medium.com/@nitinpatel_20236/how-to-shuffle-correctly-shuffle-an-array-in-javascript-15ea3f84bfb
       for (var i = articleLinks.length-1; i > 0; i--) {
         var j = Math.floor(Math.random() * i);
-        var temp_link = articleLinks[i];
+        var temp = articleLinks[i];
         articleLinks[i] = articleLinks[j];
-        articleLinks[j] = temp_link;
+        articleLinks[j] = temp;
       }
 
-      // send data to page
+      // send data to page to render
       res.render('index', {
-        initialArticle: initialArticle,
-        links: articleLinks,
+        chain: chain,
+        articleLinks: articleLinks,
         error: null
       });
 
     }
   });
+}
+
+// load page
+app.get('/', function(req, res) {
+  renderData(randomURL, res);
+});
+
+// respond to user action
+app.post('/', function(req, res) {
+  var url = 'https://en.wikipedia.org/wiki/' + req.body.article_link;
+  renderData(url, res);
 });
 
 // start server
