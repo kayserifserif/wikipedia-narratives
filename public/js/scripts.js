df4733ea31034538b35b1a73eb118e32b71e99ee
whykatherine <whykatherine@gmail.com>
2019-11-12 19:55:37 -0800

    fix: bugs and transitions

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -14,25 +14,42 @@ window.onload = function() {
     linksFadeIn();
   }
 
-  // make "create a narrative" button appear
-  var createNarrativeBtn = document.getElementById("createNarrative");
-  createNarrativeBtn.addEventListener("click", function() {
-    // hide chain counter and path form
-    document.getElementById("chain-counter").style.display = "none";
-    document.getElementById("pathForm").style.display = "none";
-    // transform article chain
-    var articleChain = document.getElementById("article-chain");
-    articleChain.classList.remove("constructing");
-    articleChain.classList.add("narrativising");
-    var nodes = document.getElementsByClassName("article-node");
-    for (var i = 0; i < nodes.length - 1; i++) {
-      // create editable text fields between nodes
-      var inputSpan = document.createElement("span");
-      inputSpan.classList.add("narrativeInput");
-      inputSpan.contentEditable = true;
-      articleChain.insertBefore(inputSpan, nodes[i].nextSibling);
+  var chainCount = document.getElementById("chain-count");
+  if (chainCount) {
+    if (parseInt(chainCount.textContent) == 10) {
+      // create "create a narrative" button
+      var createNarrativeBtn = document.createElement("button");
+      createNarrativeBtn.id = "createNarrative";
+      createNarrativeBtn.append(document.createTextNode("Create a narrative"));
+      document.body.append(createNarrativeBtn);
+      createNarrativeBtn.addEventListener("click", function() {
+        console.log("click");
+        // hide chain counter
+        document.getElementById("chain-counter").style.display = "none";
+        // transform article chain
+        var articleChain = document.getElementById("article-chain");
+        articleChain.classList.remove("constructing");
+        articleChain.classList.add("narrativising");
+        var nodes = document.getElementsByClassName("article-node");
+        for (var i = 0; i < nodes.length - 1; i++) {
+          // create editable text fields between nodes
+          var inputSpan = document.createElement("span");
+          inputSpan.classList.add("narrativeInput");
+          inputSpan.contentEditable = true;
+          articleChain.insertBefore(inputSpan, nodes[i].nextSibling);
+        }
+        // insert period
+        articleChain.append(document.createTextNode("."));
+
+        // create "compare narratives" button
+        var compareNarrativesBtn = document.createElement("button");
+        compareNarrativesBtn.id = "compareNarratives";
+        compareNarrativesBtn.append(document.createTextNode("Compare narratives"));
+        document.body.append(compareNarrativesBtn);
+
+        // remove self
+        this.parentNode.removeChild(this);
+      });
     }
-    // insert period
-    articleChain.insertBefore(document.createTextNode("."), nodes[nodes.length - 1].nextSibling);
-  });
+  }
 };
