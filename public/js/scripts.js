011e3e62ef261c0b4a2ec838d3d0d30d885c92ec
whykatherine <whykatherine@gmail.com>
2019-11-12 18:33:17 -0800

    feat: create narrative out of  article chain

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,14 +1,38 @@
 window.onload = function() {
-  var links = document.getElementsByClassName('article-link');
+  var links = document.getElementsByClassName("article-link");
   // staggered transitions code from
   // https://stackoverflow.com/a/19072639
-  var currentLink = 0;
-  var linksFadeIn = function() {
-    links[currentLink].style.opacity = 1;
-    currentLink++;
-    if (currentLink < links.length) {
-      setTimeout(linksFadeIn, 1);
+  if (links.length > 0) {
+    var currentLink = 0;
+    var linksFadeIn = function() {
+      links[currentLink].style.opacity = 1;
+      currentLink++;
+      if (currentLink < links.length) {
+        setTimeout(linksFadeIn, 1);
+      }
     }
+    linksFadeIn();
   }
-  linksFadeIn();
+
+  // make "create a narrative" button appear
+  var createNarrativeBtn = document.getElementById("createNarrative");
+  createNarrativeBtn.addEventListener("click", function() {
+    // hide chain counter and path form
+    document.getElementById("chain-counter").style.display = "none";
+    document.getElementById("pathForm").style.display = "none";
+    // transform article chain
+    var articleChain = document.getElementById("article-chain");
+    articleChain.classList.remove("constructing");
+    articleChain.classList.add("narrativising");
+    var nodes = document.getElementsByClassName("article-node");
+    for (var i = 0; i < nodes.length - 1; i++) {
+      // create editable text fields between nodes
+      var inputSpan = document.createElement("span");
+      inputSpan.classList.add("narrativeInput");
+      inputSpan.contentEditable = true;
+      articleChain.insertBefore(inputSpan, nodes[i].nextSibling);
+    }
+    // insert period
+    articleChain.insertBefore(document.createTextNode("."), nodes[nodes.length - 1].nextSibling);
+  });
 };
