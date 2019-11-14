0533b1f5258e17063d91b7ac8baf9122e51e1378
whykatherine <whykatherine@gmail.com>
2019-11-13 17:27:39 -0800

    refactor: make event handler functions separate

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,4 +1,44 @@
+var createNarrativeBtn;
+var compareNarrativesBtn;
+
+function createNarrative() {
+  // hide chain counter
+  document.getElementById("chain-counter").style.display = "none";
+  // transform article chain
+  var articleChain = document.getElementById("article-chain");
+  articleChain.classList.remove("constructing");
+  articleChain.classList.add("narrativising");
+  var nodes = document.getElementsByClassName("article-node");
+  for (var i = 0; i < nodes.length - 1; i++) {
+    // create editable text fields between nodes
+    var inputSpan = document.createElement("span");
+    inputSpan.classList.add("narrativeInput");
+    inputSpan.contentEditable = true;
+    inputSpan.onfocus = selectAllOnFocus;
+    articleChain.insertBefore(inputSpan, nodes[i].nextSibling);
+  }
+  document.getElementsByClassName("narrativeInput")[0].focus();
+  // insert period
+  articleChain.append(document.createTextNode("."));
+
+  // remove self
+  // this.parentNode.removeChild(this);
+  this.style.display = "none";
+    compareNarrativesBtn.style.display = "block";
+}
+
+function selectAllOnFocus() {
+  // this.select();
+  // document.execCommand("selectAll", true, null);
+}
+
+function compareNarratives() {
+  var paragraphs = document.getElementById("paragraphs");
+  paragraphs.style.display = "block";
+}
+
 window.onload = function() {
+
   var links = document.getElementsByClassName("article-link");
   // staggered transitions code from
   // https://stackoverflow.com/a/19072639
@@ -15,46 +55,23 @@ window.onload = function() {
   }
 
   // create "create a narrative" button
-  var createNarrative = document.getElementById("create-narrative");
-  if (createNarrative) {
-    createNarrative.addEventListener("click", function() {
-      // hide chain counter
-      document.getElementById("chain-counter").style.display = "none";
-      // transform article chain
-      var articleChain = document.getElementById("article-chain");
-      articleChain.classList.remove("constructing");
-      articleChain.classList.add("narrativising");
-      var nodes = document.getElementsByClassName("article-node");
-      for (var i = 0; i < nodes.length - 1; i++) {
-        // create editable text fields between nodes
-        var inputSpan = document.createElement("span");
-        inputSpan.classList.add("narrativeInput");
-        inputSpan.contentEditable = true;
-        articleChain.insertBefore(inputSpan, nodes[i].nextSibling);
-      }
-      // insert period
-      articleChain.append(document.createTextNode("."));
-
-      // remove self
-      // this.parentNode.removeChild(this);
-      this.style.display = "none";
-      compareNarratives.style.display = "block";
-    });
+  createNarrativeBtn = document.getElementById("create-narrative");
+  if (createNarrativeBtn) {
+    createNarrativeBtn.addEventListener("click", createNarrative);
   }
 
   // create "compare narratives" button
-  var compareNarratives = document.getElementById("compare-narratives");
-  if (compareNarratives) {
-    compareNarratives.addEventListener("click", function() {
-      var paragraphs = document.getElementById("paragraphs");
-      paragraphs.style.display = "block";
-    });
+  compareNarrativesBtn = document.getElementById("compare-narratives");
+  if (compareNarrativesBtn) {
+    compareNarrativesBtn.addEventListener("click", compareNarratives);
   }
 
+  // display "create narratives" button
   var chainCount = document.getElementById("chain-count");
   if (chainCount) {
     if (parseInt(chainCount.textContent) == 10) {
-      createNarrative.style.display = "block";
+      createNarrativeBtn.style.display = "block";
     }
   }
+
 };
