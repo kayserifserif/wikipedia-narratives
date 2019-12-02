261ef310f8d55a5974e13f879a9b030440082f7b
whykatherine <whykatherine@gmail.com>
2019-12-02 12:30:57 -0800

    chore: force https

--- server.js
+++ server.js
@@ -142,6 +142,16 @@ function renderChain(url, isManual, res) {
   });
 }
 
+if (process.env.NODE_ENV === "production") {
+  app.use((req, res, next) => {
+    if (req.header("x-forwarded-proto") !== "https") {
+      res.redirect(`https://${req.header('host')}${req.url}`);
+    } else {
+      next();
+    }
+  });
+} 
+
 // load start page
 app.get('/', function(req, res) {
   res.render('index');
